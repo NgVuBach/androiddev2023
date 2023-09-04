61d637f4f6d5e69c218bd9ca9bee5337816b456a
NgVuBach <bachnv.bi12-044@st.usth.edu.vn>
2023-09-04 14:29:08 +0700

    Practical 4

--- app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -4,13 +4,20 @@ import static vn.edu.usth.weather.R.*;
 
 import android.annotation.SuppressLint;
 import android.app.ActivityManager;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,52 +25,44 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ForecastFragment extends Fragment {
-    
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public ForecastFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ForecastFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ForecastFragment newInstance(String param1, String param2) {
         ForecastFragment fragment = new ForecastFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Create a vertical LinearLayout
+        LinearLayout view = new LinearLayout(getActivity());
+        view.setOrientation(LinearLayout.VERTICAL);
+        view.setBackgroundColor(Color.parseColor("#20FF0000"));
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(layout.fragment_forecast, container, false);
+        // Add image
+        ImageView img = new ImageView(getActivity());
+        img.setImageResource(drawable.sunny);
+        img.setPivotY(Gravity.CENTER);
+        img.setLayoutParams(new LinearLayout.LayoutParams(144, 144));
+
+        // Add text
+        TextView text = new TextView(getActivity());
+        text.setText("Thursday");
+
+        // Add text and image to linearlayout
+        view.addView(text);
+        view.addView(img);
+
+        // Return view
+        return view;
     }
 }
